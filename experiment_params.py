fixed wrong index bugs in Li and Pullen model

experiment_params.py
@@ -6,29 +6,39 @@ import numpy as np
 
 # We should also have info about noise here.
 
-cosmology = 'Planck13'
-#n = 30
-#x = np.linspace(0, 1, n + 1)
-#y = np.linspace(0, 1, n + 1)
-#z = np.linspace(0, 1, n + 1)
+cosmology = 'Planck15'  # must be an astropy compatible cosmology
 
-FWHM = 4  # arcmin
+#FWHM = 4  # arcmin Haavard
+FWHM = 6  # arcmin Li
+
 n_pix_x = 22  # no of pixels
 n_pix_y = 22
 
 # should be calculated later
-sigma_T = 1e6  # muK, noise
-
+#sigma_T = 11.#2.75#1e9  # muK, noise Haavard
+sigma_T = 23.25# MuK, Li
 map_smoothing = True
 
 halo_catalogue_file = 'catalogues/peakpatchcatalogue_1pt4deg_z2pt4-3pt4.npz'
+#halo_catalogue_file = 'full_catalogues/COMAP_z2.39-3.44_1140Mpc_seed_13579.npz'
+#halo_catalogue_file = 'full_catalogues/COMAP_z2.39-3.44_1140Mpc_seed_13581.npz'
+#halo_catalogue_file = 'full_catalogues/COMAP_z2.39-3.44_1140Mpc_seed_13583.npz'
+#halo_catalogue_file = 'full_catalogues/COMAP_z2.39-3.44_1140Mpc_seed_13585.npz'
+
 min_mass = 2.5e11
 
 # field of view in degrees, field size
 fov_x = 1.4
 fov_y = 1.4
 
-n_nu_bins = 32  # number of frequency bins 2^r
+# Pullen
+#n_nu_bins = 512  # number of frequency bins 2^r
+#nu_rest = 115.27  # rest frame frequency of CO(1-0) transition in GHz
+#nu_i = 34.    # GHz
+#nu_f = 26.
+
+# Li
+n_nu_bins = 100  # 100 number of frequency bins 2^r
 nu_rest = 115.27  # rest frame frequency of CO(1-0) transition in GHz
 nu_i = 34.    # GHz
-nu_f = 26.
+nu_f = 30.

mcmc_params.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 n_walkers = 10
-n_steps = 200
+n_steps = 300
 
 # number of independent simulations of the model for each
 # step in the MCMC
@@ -14,14 +14,15 @@ observables = ('ps', 'vid')
 #observables =('vid')
 #model = 'wn_ps'
 #model = 'pl_ps'
-model = 'Lco_Pullen'
+#model = 'Lco_Pullen'
+model = 'Lco_Li'
 
 prior_params = dict()
 
 #ps_kbins = np.logspace(1, 2, 10)
 #vid_Tbins = np.logspace(2,3, 11)
 ps_kbins = np.logspace(-1.5, -0.1, 10)
-vid_Tbins = np.logspace(5.5, 6.7, 10)
+vid_Tbins = np.logspace(1, 2, 10)
 #vid_Tbins = np.logspace(5.7, 8, 10)  # Lco, 10x10x10
 
 # Gaussian prior for white noise power spectrum
@@ -30,6 +31,7 @@ prior_params['wn_ps'] = [
 ]
 
 # Gaussian prior for power law power spectrum
+ #[mean, stddev]
 prior_params['pl_ps'] = [
     [7.0, 3.0],  # A
     [2.5, 1.7]  # alpha
@@ -38,7 +40,15 @@ prior_params['pl_ps'] = [
 # Gaussian prior for linear L_CO model
 prior_params['Lco_Pullen'] = [
     #[3, 2]
-    [np.log10(4e-5), 1.7]  # A
+    [np.log10(4e-6), 1.]  # A
+]
+
+prior_params['Lco_Li'] = [
+    [0.0, 0.3], 	# logdelta_MF
+    [1.17, 0.37],	 # alpha - log10 slope
+    [0.21, 3.74],	 # beta - log10 intercept
+    [0.3, 0.1],		# sigma_SFR
+    [0.3, 0.1],		# sigma_Lco
 ]
 
 
@@ -46,9 +56,11 @@ model_params_true = dict()
 model_params_true['wn_ps'] = [8.3]  # sigma_T for wn_ps
 model_params_true['pl_ps'] = [8., 2.]  # A and alpha for pw_ps
 model_params_true['Lco_Pullen'] = [np.log10(1e6/5e11)]  # linear model, specify None for default coeffs
+model_params_true['Lco_Li'] = [0.0, 1.37, -1.74, 0.3, 0.3]  # model, specify None for default coeffs
 
-map_filename = 'trial2.npy'
-samples_filename = 'samples_lco_test2.npy'
+
+map_filename = 'trial3.npy'
+samples_filename = 'samples_lco_test3.npy'
 
 output_dir = 'testing_output'
 generate_file = True

run_mcmc.py
@@ -21,7 +21,8 @@ import experiment_params as exp_params
 import emcee
 
 #from schwimmbad import MPIPool
-#from multiprocessing import Pool
+from emcee.utils import MPIPool
+from multiprocessing import Pool
 import sys
 import os
 import datetime
@@ -29,14 +30,22 @@ import datetime
 os.environ["OMP_NUM_THREADS"] = "1"
 # Perhaps move this function somewhere else ?
 
-
+#print('running')
+#print(mcmc_params.model_params_true['Lco_test'])
 def set_up_mcmc(mcmc_params, exp_params):
     """
     Sets up the different objects for the mcmc-run.
     """
 
     observables = []
-    map_obj = src.MapObj.MapObj(exp_params)
+    model_name = mcmc_params.model
+    halos, cosmo = src.tools.load_peakpatch_catalogue(
+                        exp_params.halo_catalogue_file)
+    map_obj = src.MapObj.MapObj(exp_params, cosmo)
+
+    # remove Halo's we don't want.
+    halos = src.tools.cull_peakpatch_catalogue(
+                halos, exp_params.min_mass, map_obj)
 
     # Add more if-statements as other observables are implemented.
     # At some point we should add some checks to make sure that a
@@ -47,18 +56,16 @@ def set_up_mcmc(mcmc_params, exp_params):
     if 'vid' in mcmc_params.observables:
         vid = src.Observable.Voxel_Intensity_Distribution(mcmc_params)
         observables.append(vid)
+
     if (mcmc_params.model == 'wn_ps'):
         model = src.Model.WhiteNoisePowerSpectrum(exp_params)
     if (mcmc_params.model == 'pl_ps'):
         model = src.Model.PowerLawPowerSpectrum(exp_params, map_obj)
-    if (mcmc_params.model == 'Lco_test'):
-        halos = src.tools.load_peakpatch_catalogue(
-                           exp_params.halo_catalogue_file)
-        # remove Halo's we don't want.
-        halos = src.tools.cull_peakpatch_catalogue(
-                    halos, exp_params.min_mass, map_obj)
 
-        model = src.Model.Mhalo_to_Lco_test(exp_params, halos, map_obj)
+    if (mcmc_params.model == 'Lco_Pullen'):
+        model = src.Model.Mhalo_to_Lco_Pullen(exp_params, halos, map_obj)
+    if (mcmc_params.model == 'Lco_Li'):
+        model = src.Model.Mhalo_to_Lco_Li(exp_params, halos, map_obj)
 
     return model, observables, map_obj
 
@@ -91,7 +98,6 @@ def lnprob(model_params, model, observables, map_obj):
                                mcmc_params.prior_params[model.label])
     if not np.isfinite(ln_prior):
         return -np.infty
-
     for i in range(mcmc_params.n_realizations):
         map_obj.map = model.generate_map(
             model_params) + map_obj.generate_noise_map()
@@ -160,13 +166,13 @@ model, observables, map_obj = set_up_mcmc(mcmc_params, exp_params)
 get_data(mcmc_params, exp_params, observables,
          model)  # np.load("ps_data.npy")
 
-# with MPIPool() as pool:
-#    if not pool.is_master():
-#        pool.wait()
-#        sys.exit(0)
+#pool = MPIPool(loadbalance=True)
+#if not pool.is_master():
+#    pool.wait()
+#    sys.exit(0)
 
 sampler = emcee.EnsembleSampler(mcmc_params.n_walkers, model.n_params, lnprob,
-                                args=(model, observables, map_obj), threads=144)  # , pool=pool)
+                                args=(model, observables, map_obj), threads=144)#, pool=pool)
 
 # starting positions (when implementing priors properly,
 # find a good way to draw the starting values from that prior.)
@@ -182,16 +188,18 @@ i = 0
 with open(mcmc_chains_fp, 'w') as chains_file:
     while i < mcmc_params.n_steps:
         print('undergoing iteration {0}'.format(i))
+        sys.stdout.flush()
         for result in sampler.sample(pos, iterations=1, storechain=True):
             samples[i], _, blobs = result
             pos = samples[i]
             chains_file.write('\n'.join([str(item) for sublist in pos for item in sublist])+'\n')
             i += 1
 
+#pool.close()
 samples = samples.reshape(mcmc_params.n_steps * mcmc_params.n_walkers,
                           model.n_params)
 
 
 np.save(mcmc_params.samples_filename, samples)
 
-src.tools.make_log_file(mcmc_log_fp, start_time)
+src.tools.write_log_file(mcmc_log_fp, start_time)

src/Model.py
@@ -2,8 +2,10 @@ import numpy as np
 from scipy.stats import norm
 import src.tools
 import sys
-import warnings
-import matplotlib.pyplot as plt
+import scipy as sp
+import os
+import scipy.interpolate
+
 
 class Model:
     """
@@ -122,52 +124,12 @@ class PowerLawPowerSpectrum(Model):
         return ln_prior
 
 
-class Mhalo_to_Lco_Pullen(Model):
-
+class Mhalo_to_Lco(Model):
     def __init__(self, exp_params, halos, map_obj):
         self.exp_params = exp_params
-        self.label = 'Lco_Pullen'
         self.halos = halos
         self.map_obj = map_obj
-        self.n_params = 1
 
-    def mcmc_walker_initial_positions(self, prior_params, n_walkers):
-        p_par = np.transpose(prior_params)
-        mean, sigma = p_par[0], p_par[0]
-
-        # Either take abs or redraw until positive value
-        init_pos = mean + sigma * np.random.randn(n_walkers, len(mean))
-
-        return init_pos
-
-    def ln_prior(self, model_params, prior_params):
-        ln_prior = 0.0
-        for m_par, p_par in zip(model_params, prior_params):
-            ln_prior += norm.logpdf(m_par,
-                                    loc=p_par[0],
-                                    scale=p_par[1])
-        return ln_prior
-
-    def calculate_Lco(self, model_param=None):  # halos, coeffs
-        """
-        Luminosity in units of L_sun, halos mass in units of M_sun
-        Coefficients in units of L_sun/M_sun
-        Default coefficients taken from Pullen et al. 2013
-        """
-        #numpy.seterr(all='warn')
-        #warnings.filterwarnings('error')
-
-        if model_param is None:
-            model_param = np.log10(1e6/5e11)
-
-        #with np.errstate(over='raise'):
-        try:
-            Lco = 10**model_param * self.halos.M
-        except OverflowError as err:
-            print('MODEL PARAM:',model_param)
-            sys.exit()
-        #print(model_param)
-        return 10**model_param * self.halos.M
 
     def T_line(self):  # map, halos
         """
@@ -184,15 +146,10 @@ class Mhalo_to_Lco_Pullen(Model):
         halos = self.halos
         map_obj = self.map_obj
         convfac = 2.63083
-        #print('lum', halos.Lco)
-        #print('nu', halos.nu)
 
         Tco = 1./2 * convfac/halos.nu**2 * halos.Lco / 4/np.pi / \
             halos.chi**2/(1 + halos.redshift)**2/map_obj.dnu/map_obj.Ompix
-        #print('Tco', Tco)
-        #plt.plot(self.map_obj.z_array, Tco[:len(map_obj.z_array)])
-        #plt.show()
-        #sys.exit()
+
         return Tco
 
     def generate_map(self, model_params):  # Lco_to_map
@@ -202,7 +159,7 @@ class Mhalo_to_Lco_Pullen(Model):
         halos = self.halos
 
         halos.nu = map_obj.nu_rest / (halos.redshift + 1)
-        halos.Lco = self.calculate_Lco(model_params[0])
+        halos.Lco = self.calculate_Lco(model_params)
         # Transform from Luminosity to Temperature
         halos.Tco = self.T_line()
 
@@ -218,7 +175,144 @@ class Mhalo_to_Lco_Pullen(Model):
         # flip back frequency bins
         if self.exp_params.map_smoothing:
             return src.tools.gaussian_smooth(
-                            maps[:, :, ::-1], self.exp_params.sigma_x,
-                            self.exp_params.sigma_y, n_sigma=5.0)
+                            maps[:, :, ::-1], map_obj.sigma_x,
+                            map_obj.sigma_y, n_sigma=5.0)
 
         return maps[:, :, ::-1]
+
+
+class Mhalo_to_Lco_Pullen(Mhalo_to_Lco):
+
+    def __init__(self, exp_params, halos, map_obj):
+        self.label = 'Lco_Pullen'
+        self.n_params = 1
+        super().__init__(exp_params, halos, map_obj)
+
+    def mcmc_walker_initial_positions(self, prior_params, n_walkers):
+        p_par = np.transpose(prior_params)
+        mean, sigma = p_par[0], p_par[1]
+        initial_pos = mean + sigma * np.random.randn(n_walkers, len(mean))
+        return initial_pos
+
+    def ln_prior(self, model_params, prior_params):
+        ln_prior = 0.0
+        for m_par, p_par in zip(model_params, prior_params):
+            ln_prior += norm.logpdf(m_par,
+                                    loc=p_par[0],
+                                    scale=p_par[1])
+        return ln_prior
+
+    def calculate_Lco(self, model_param=None):  # halos, coeffs
+        """
+        Luminosity in units of L_sun, halos mass in units of M_sun
+        Coefficients in units of L_sun/M_sun
+        Default coefficients taken from Pullen et al. 2013
+        """
+        model_param = model_param[0]
+        if model_param is None:
+            model_param = np.log10(1e6/5e11)
+
+        Lco = 10**model_param * self.halos.M
+        return Lco
+
+
+sfr_interp_tab = None
+
+
+class Mhalo_to_Lco_Li(Mhalo_to_Lco):
+
+    def __init__(self, exp_params, halos, map_obj):
+        self.label = 'Lco_Li'
+        self.n_params = 5
+        super().__init__(exp_params, halos, map_obj)
+
+    def mcmc_walker_initial_positions(self, prior_params, n_walkers):
+        p_par = np.transpose(prior_params)
+        mean, sigma = p_par[0], p_par[1]
+        initial_pos = mean + sigma * np.random.randn(n_walkers, len(mean))
+        initial_pos[:, 3:] = np.abs(initial_pos[:, 3:])
+        return initial_pos
+
+    def ln_prior(self, model_params, prior_params):
+        ln_prior = 0.0
+        if (model_params[3] < 0) or (model_params[4] < 0):
+            return - np.infty
+        for m_par, p_par in zip(model_params, prior_params):
+            print('par', p_par[0], p_par[1])
+            ln_prior += norm.logpdf(m_par,
+                                    loc=p_par[0],
+                                    scale=p_par[1])
+        return ln_prior
+
+    def calculate_Lco(self, model_params=None):
+        global sfr_interp_tab
+        halos = self.halos
+
+        if model_params is None:
+            # Power law parameters from paper
+            log_delta_mf, alpha, beta, sigma_sfr, sigma_lco = [
+                0.0, 1.37, -1.74, 0.3, 0.3]
+        else:
+            log_delta_mf, alpha, beta, sigma_sfr, sigma_lco = model_params
+        delta_mf = 10**log_delta_mf
+
+        # Get Star formation rate
+        if sfr_interp_tab is None:
+            sfr_interp_tab = self.get_sfr_table()
+        sfr = sfr_interp_tab.ev(np.log10(halos.M), np.log10(halos.redshift+1))
+        sfr = self.add_log_normal_scatter(sfr, sigma_sfr)
+        # infrared luminosity
+        lir = sfr * 1e10 / delta_mf
+        alphainv = 1./alpha
+        # Lco' (observers units
+        #overflow in either of these
+        lir_ =lir**alphainv
+        beta_ = 10**(-beta * alphainv)
+
+        Lcop = lir_ * beta_
+        Lco = 4.9e-5 * Lcop
+        Lco = self.add_log_normal_scatter(Lco, sigma_lco)
+
+        return Lco
+
+    @staticmethod
+    def get_sfr_table():
+        """
+        Load SFR Table
+        Columns are: z+1, logmass, logsfr, logstellarmass
+        Intermediate processing of tabulated data
+        """
+
+        tablepath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        tablepath += '/tables/sfr_behroozi_release.dat'
+        dat_zp1, dat_logm, dat_logsfr, _ = np.loadtxt(tablepath, unpack=True)
+
+        dat_logzp1 = np.log10(dat_zp1)
+        dat_sfr = 10.**dat_logsfr
+
+        # Reshape arrays
+        dat_logzp1 = np.unique(dat_logzp1)    # log(z), 1D
+        dat_logm = np.unique(dat_logm)    # log(Mhalo), 1D
+        dat_sfr = np.reshape(dat_sfr, (dat_logm.size, dat_logzp1.size))
+
+        # Get interpolated SFR value(s)
+        sfr_interp_tab = sp.interpolate.RectBivariateSpline(dat_logm, dat_logzp1, dat_sfr,
+                                                            kx=1, ky=1)
+        return sfr_interp_tab
+
+    @staticmethod
+    def add_log_normal_scatter(data, dex):
+        """
+        Return array x, randomly scattered by a log-normal distribution with sigma=dexscatter.
+        [via @tonyyli - https://github.com/dongwooc/imapper2]
+        Note: scatter maintains mean in linear space (not log space).
+        """
+        if (dex <= 0):
+            return data
+        # Calculate random scalings
+        sigma = dex * 2.302585  # Stdev in log space (DIFFERENT from stdev in linear space), note: ln(10)=2.302585
+        mu = -0.5*sigma**2
+
+        randscaling = np.random.lognormal(mu, sigma, data.shape)
+        xscattered = np.where(data > 0, data*randscaling, data)
+        return xscattered

src/likelihoods.py
@@ -6,5 +6,9 @@ def ln_chi_squared(data, avg, sigma):
     #print('sigma', sigma)
     #sys.exit()
 
-    like = -1.0 / 2 * np.sum((data - avg) ** 2 / (2 * sigma ** 2))
+    #square1 = (data - avg) ** 2
+    #square2 = (2 * sigma ** 2)
+    #like = -1.0 / 2 * np.sum(square1 / square2)
+
+    like = -1.0 / 2 * np.sum(1./2*((data - avg) / sigma)**2)
     return like

src/tools.py
@@ -63,6 +63,8 @@ def calculate_power_spec_3d(map_obj, k_bin=None):
 
     fft_map = fft.fftn(map_obj.map) / (map_obj.n_x * map_obj.n_y * map_obj.n_z)
     #fft_map = fft.fftshift(fft_map)
+
+    # overflow in square sometimes
     ps = np.abs(fft_map)**2 * map_obj.volume
 
     Pk_modes = np.histogram(
@@ -85,13 +87,14 @@ def calculate_vid(map_obj, T_bin=None):
                             np.amax(map_obj.map) + 1)
     try:
         B_val, T_edges = np.histogram(map_obj.map.flatten(), bins=T_bin)
+        B_val = B_val.astype(float)
         B_val[np.where(B_val == 0)]= 1e-3
         T_array = (T_edges[1:] + T_edges[:-1]) / 2.
         return B_val, T_array
+
     except ValueError:
-        print('T_bin=', T_bin)
         print('map=', map_obj.map)
-        print('wrong')
+        print('wrong in vid calculation')
         sys.exit()
 
 
@@ -178,11 +181,18 @@ def load_peakpatch_catalogue(filein):
         Contains all halo information (position, redshift, etc..)
     """
     halos = empty_table()            # creates empty class to put any halo info into
+    cosmo = empty_table()            # creates empty class to put any cosmology info into  
 
     halo_info = np.load(filein)
 
     # get cosmology from halo catalogue
     params_dict = halo_info['cosmo_header'][()]
+    cosmo.Omega_M = params_dict.get('Omega_M')
+    cosmo.Omega_B = params_dict.get('Omega_B')
+    cosmo.Omega_L = params_dict.get('Omega_L')
+    cosmo.h = params_dict.get('h')
+    cosmo.ns = params_dict.get('ns')
+    cosmo.sigma8 = params_dict.get('sigma8')
 
     # if the halo catalogue is not centered along the z axis
     cen_x_fov = params_dict.get('cen_x_fov', 0.)
@@ -207,7 +217,7 @@ def load_peakpatch_catalogue(filein):
 
     assert np.max(halos.M) < 1.e17, "Halos seem too massive"
 
-    return halos
+    return halos, cosmo
 
 
 def cull_peakpatch_catalogue(halos, min_mass, mapinst):